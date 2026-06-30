f5750b49c3d689eca82cfbfe492dd865107cf995
it-mobstudio <137990018+it-mobstudio@users.noreply.github.com>
2026-06-30 16:41:27 +0530

    UI fixes

--- lib/features/home/presentation/widgets/home_promo_banner.dart
+++ lib/features/home/presentation/widgets/home_promo_banner.dart
@@ -18,57 +18,26 @@ class HomePromoBanner extends StatelessWidget {
           fit: StackFit.expand,
           children: [
             const ColoredBox(color: Color(0xFF3D0E09)),
-            Row(
-              children: [
-                Expanded(
-                  child: Transform.translate(
-                    offset: const Offset(0, 1),
-                    child: SizedBox(
-                      height: 100,
-                      child: Image.asset(
-                        'assets/images/Leftside.png',
-                        fit: BoxFit.cover,
-                        alignment: Alignment.bottomLeft,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Transform.translate(
-                    offset: const Offset(0, 1),
-                    child: SizedBox(
-                      height: 100,
-                      child: Image.asset(
-                        'assets/images/Rightside.png',
-                        fit: BoxFit.cover,
-                        alignment: Alignment.bottomRight,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            Image.asset(
+              'assets/images/bannerBG.png',
+              fit: BoxFit.cover,
             ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                height: 75,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment(0.50, 0.00),
-                    end: Alignment(0.50, 1.00),
-                    colors: [
-                      Color(0xFF121212),
-                      Color(0x00131313),
-                    ],
-                  ),
+            const DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF131313),
+                    Color(0x00131313),
+                  ],
                 ),
               ),
             ),
             Center(
               child: SvgPicture.asset(
                 'assets/images/Qwiklogo.svg',
-                height: 44,
+                height: 68,
                 fit: BoxFit.contain,
               ),
             ),
